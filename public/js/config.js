d23e504f334e71db22e1eef8e036c23118b124cd
N-Ment <93761044+N-Ment@users.noreply.github.com>
2026-09-25 09:56:43 +0700

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+// ใส่ URL ของ Apps Script Web App ที่นี่ หลังจาก Deploy > New deployment > Web app
+// ตัวอย่าง: https://script.google.com/macros/s/xxxxxxxxxxxxxxxx/exec
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxin_KEu-6y4SmWCwCrG1J_2sXpuQ31SDNBfTqkw8PMsnS6PpgLJzKIacyvPsn0LfXGwA/exec';
+window.SHOP_NAME = 'แม่ขนตางอน';
